149430e42dedab46eb9ea52f321c9547bcfd18c1
Nils <ndrebing@uni-bremen.de>
2017-12-11 00:56:38 +0100

    login now thorws error

--- music/forms.py
+++ music/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class SignUpForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
-    passwort = forms.CharField(label='password', max_length=100)
+    password = forms.CharField(label='password', max_length=100)
