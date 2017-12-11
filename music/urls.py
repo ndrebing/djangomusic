60b77a4420a488e6ca8534158c9fb4790cc13cad
skuehl <skuehl@tzi.de>
2017-12-11 01:28:33 +0100

    a

--- music/urls.py
+++ music/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login_action, name='login_action'),
-    path('logout', views.logout_action, name='logout_action'),
+    path('login', views.login_action, name='login'),
+    path('logout', views.logout_action, name='logout'),
     path('play_music', views.play_music, name='play_music'),
     path('<int:playlistitem_id>/', views.detail, name='detail'),
     path('ajax/add_youtube_url/', views.add_youtube_url, name='add_youtube_url'),
